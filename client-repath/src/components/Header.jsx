9b4bb8e5a7b96368079b9621f83053c651f1fcdb
azmifitra <azmi801@gmail.com>
2022-01-18 15:27:44 +0700

    check github pull data

---
+++ client-repath/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Avatar from '@mui/material/Avatar';
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        {/* <img src="https://images.unsplash.com/photo-1642412446515-779fe60b1e03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" /> */}
+        <div
+          style={{
+            height: '250px',
+            backgroundImage: "url('https://images.unsplash.com/photo-1642412446515-779fe60b1e03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')",
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        >
+          <div style={{ paddingTop: '130px', paddingLeft: '100px' }}>
+            <Avatar alt="Zemy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 70, height: 70 }} />
+          </div>
+          <span className="dot-header"></span>
+          <div class="vl-header"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
