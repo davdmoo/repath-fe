a4e8a3e301ec7166d235146c2a328199f43b69bf
davdmoo <david.mulyawan97@gmail.com>
2022-01-22 20:34:10 +0700

    Comment done

--- client-repath/src/components/componentsChild/ModalComment.jsx
+++ client-repath/src/components/componentsChild/ModalComment.jsx
@@ -3,12 +3,33 @@ import { IconButton, TextField, Button } from '@mui/material/';
 import { Modal } from 'react-bootstrap';
 import { Comment, Send } from '@mui/icons-material/';
 import { blue } from '@mui/material/colors';
+import { useDispatch } from 'react-redux';
+import { commentPost } from '../../store/actionCreators/postCreator';
 
-function ModalComment() {
+function ModalComment({ post }) {
   const [show, setShow] = useState(false);
+  const dispatch = useDispatch();
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [content, setContent] = useState("");
+
+  function handleComment(e) {
+    e.preventDefault();
+    const commentPayload = {
+      id: post._id,
+      content
+    }
+    dispatch(commentPost(commentPayload))
+      .then(() => {
+        setShow(false);
+      })
+  }
+
+  function handleContent(e) {
+    setContent(e.target.value);
+  }
 
   return (
     <>
@@ -17,17 +38,19 @@ function ModalComment() {
       </IconButton>
       {/* ======================================== MODAL ========================================*/}
       <Modal show={show} onHide={handleClose} centered size="md">
-        <Modal.Header closeButton>
-          <Modal.Title>Comment</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <TextField fullWidth id="standard-basic" label="Standard" variant="standard" />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="contained" endIcon={<Send />}>
-            Post
-          </Button>
-        </Modal.Footer>
+        <form onSubmit={handleComment}>
+          <Modal.Header closeButton>
+            <Modal.Title>Comment</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <TextField value={content} onChange={handleContent} fullWidth id="standard-basic" label="Standard" variant="standard" />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="submit" variant="contained" endIcon={<Send />}>
+              Post
+            </Button>
+          </Modal.Footer>
+        </form>
       </Modal>
     </>
   );
