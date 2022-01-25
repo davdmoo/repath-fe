78f50e7fb43b132ed01d7f47faabdff58b7e27c6
Agnes Suryani <el.agnesurya@gmail.com>
2022-01-25 19:49:57 +0700

    google login

--- client-repath/src/views/TestGoogle.jsx
+++ client-repath/src/views/TestGoogle.jsx
@@ -1,50 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { GoogleLogin } from 'react-google-login';
+import {setGoogleLogin} from '../store/actionCreators/userCreator'
 
 function TestGoogle() {
+  const dispatch = useDispatch();
+
   const responseGoogle = (response) => {
     console.log(response);
+    dispatch(setGoogleLogin(response));
   };
-  //   const dispatch = useDispatch();
-  //   const { posts, postsLoading, postsError } = useSelector((state) => state.postReducer);
-
-  //   const [currentUser, setcurrentUser] = useState({
-  //     firstName: '',
-  //     lastName: '',
-  //     email: '',
-  //     header: '',
-  //     imgUrl: '',
-  //   });
-
-  //   useEffect(() => {
-  //     dispatch(fetchUserById(localStorage.id)).then(({ data }) => {
-  //       setcurrentUser({
-  //         firstName: data.firstName,
-  //         lastName: data.lastname,
-  //         email: data.email,
-  //         imgUrl: data.imgUrl,
-  //         header: data.header,
-  //       });
-  //     });
-  //   }, []);
-
-  //   useEffect(() => {
-  //     dispatch(fetchPosts());
-  //   }, []);
-
-  //   if (postsError) {
-  //     return (
-  //       <>
-  //         <Navbar />
-  //         <ErrorGlobal />
-  //       </>
-  //     );
-  //   }
+  
 
   return (
     <>
       <h1>COBA COBA GOOGLE LOGIN</h1>
-      <GoogleLogin clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com" buttonText="Login" onSuccess={responseGoogle} onFailure={responseGoogle} cookiePolicy={'single_host_origin'} />
+      <GoogleLogin clientId="306277501455-3tep6b17k5avj734itbeqju5g6asoind.apps.googleusercontent.com" buttonText="Login" onSuccess={responseGoogle} onFailure={responseGoogle} cookiePolicy={'single_host_origin'} />
     </>
   );
 }
