385c21739b03ee38917bd3969f023c340c85f5e4
azmifitra <azmi801@gmail.com>
2022-01-25 19:52:46 +0700

    sandbox infinite scroll

--- client-repath/src/components/CardFriendList.jsx
+++ client-repath/src/components/CardFriendList.jsx
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Avatar } from '@mui/material/';
-import { Card } from 'react-bootstrap';
+import { Avatar, IconButton } from '@mui/material/';
+import { Card, Modal, Button } from 'react-bootstrap';
 import { red } from '@mui/material/colors';
 import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
 import { useDispatch, useSelector } from 'react-redux';
-import { delFriendReq } from '../store/actionCreators/followCreator';
-import { followUser, addFriend, fetchFollowing } from '../store/actionCreators/followCreator';
+import { delFriendReq, delOneFriend } from '../store/actionCreators/followCreator';
 
 function CardFriendList({ user }) {
   const dispatch = useDispatch();
 
   const handleDelFriend = (reqId) => {
-    dispatch(delFriendReq(reqId));
+    dispatch(delOneFriend(reqId));
+  };
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const doDelete = (postId) => {
+    // dispatch(deletePost(postId)).then(() => {
+    // warnToastAlert('Success deleted post!', 'top-center', 3000, false);
+    // });
   };
 
   // const { following } = useSelector((state) => state.userReducer);
@@ -19,30 +29,60 @@ function CardFriendList({ user }) {
   //   dispatch(fetchFollowing());
   // }, []);
   return (
-    <Card style={{ border: '0px' }}>
-      <Card.Body style={{ backgroundColor: '#fef2f2', padding: '0px' }}>
-        <div className="card-container-friendlist">
-          <div className="card-left-side-friendlist d-flex align-items-center">
-            {user.imgUrl ? (
-              <Avatar className="avatar-card" alt="David" src={user.imgUrl} sx={{ width: 60, height: 60 }} variant="rounded"></Avatar>
-            ) : (
-              <Avatar className="avatar-card" alt={user.firstName} src="/static/images/avatar/1.jpg" sx={{ width: 60, height: 60 }} variant="rounded"></Avatar>
-            )}
-          </div>
-          <div className="card-right-side-friendlist d-flex flex-row align-items-center">
-            <div className="content-friendlist" style={{ width: '200px', textAlign: 'left' }}>
-              <div className="friendlist-name">
-                {user.firstName} {user.lastName}
+    <>
+      <Card style={{ border: '0px' }}>
+        <Card.Body style={{ backgroundColor: '#fef2f2', padding: '0px' }}>
+          <div className="card-container-friendlist">
+            <div className="card-left-side-friendlist d-flex align-items-center">
+              {user.imgUrl ? (
+                <Avatar className="avatar-card" alt="David" src={user.imgUrl} sx={{ width: 60, height: 60 }} variant="rounded"></Avatar>
+              ) : (
+                <Avatar className="avatar-card" alt={user.firstName} src="/static/images/avatar/1.jpg" sx={{ width: 60, height: 60 }} variant="rounded"></Avatar>
+              )}
+            </div>
+            <div className="card-right-side-friendlist d-flex flex-row align-items-center">
+              <div className="content-friendlist" style={{ width: '200px', textAlign: 'left' }}>
+                <div className="friendlist-name">
+                  {user.firstName} {user.lastName}
+                </div>
+                <div className="friendlist-city">{user.city}</div>
               </div>
-              <div className="friendlist-city">{user.city}</div>
+            </div>
+            <div style={{ width: '100px' }} className="d-flex justify-content-center align-items-center">
+              <IconButton onClick={handleShow}>
+                <PersonRemoveIcon sx={{ width: 35, height: 35, color: red[500] }} />
+              </IconButton>
             </div>
           </div>
-          <div style={{ width: '100px' }} className="d-flex justify-content-center align-items-center">
-            <PersonRemoveIcon onClick={() => handleDelFriend(user.phoneNumber)} sx={{ width: 35, height: 35, color: red[500] }} />
-          </div>
-        </div>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Unfriend with{' '}
+          <span className="fw-bold">
+            {user.firstName} {user.lastName}
+          </span>{' '}
+          ?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="outline-warning" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            variant="danger"
+            onClick={(e) => {
+              e.preventDefault();
+              handleDelFriend(user.phoneNumber);
+            }}
+          >
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
--- client-repath/src/views/TestGoogle.jsx
+++ client-repath/src/views/TestGoogle.jsx
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleLogin } from 'react-google-login';
+import { fetchPosts } from '../store/actionCreators/postCreator';
+import { useDispatch, useSelector } from 'react-redux';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function TestGoogle() {
   const responseGoogle = (response) => {
     console.log(response);
+  };
+
+  const dispatch = useDispatch();
+  const { posts, postsLoading, postsError } = useSelector((state) => state.postReducer);
+
+  useEffect(() => {
+    // dispatch(fetchPosts());
+  }, []);
+
+  const [items, setItems] = useState(posts);
+
+  const fetchMoreData = () => {
+    setItems(items.concat(posts));
   };
   //   const dispatch = useDispatch();
   //   const { posts, postsLoading, postsError } = useSelector((state) => state.postReducer);
@@ -42,10 +58,19 @@ function TestGoogle() {
   //   }
 
   return (
-    <>
+    <div style={{ backgroundColor: 'blue', height: '100vh' }}>
       <h1>COBA COBA GOOGLE LOGIN</h1>
       <GoogleLogin clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com" buttonText="Login" onSuccess={responseGoogle} onFailure={responseGoogle} cookiePolicy={'single_host_origin'} />
-    </>
+      <div style={{ height: '100%', overflowY: 'hidden' }}>
+        <InfiniteScroll dataLength={items.length} next={fetchMoreData} hasMore={true} loader={<h4>Loading...</h4>}>
+          {items.map((i, index) => (
+            <div key={index}>
+              div - #{index} {i._id}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+    </div>
   );
 }
 
