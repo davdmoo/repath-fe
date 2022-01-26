7178849e9913a71677a9c89b1fa9709d2d35a26c
siska <siska25.oktaviani@gmail.com>
2022-01-26 18:50:47 +0700

    try merge

--- client-repath/src/App.js
+++ client-repath/src/App.js
@@ -15,11 +15,8 @@ import PostSearchLocation from './views/PostSearchLocation';
 import FollowerPage from './views/FollowerPage';
 import RequestPage from './views/RequestPage';
 import TestGoogle from './views/TestGoogle';
-<<<<<<< HEAD
 import LikedPage from './views/LikedPost';
-=======
 import SandboxInfinite from './views/SandboxInfinite';
->>>>>>> b388d509f2790f6d8fcc670e8476111e839efe75
 
 function App() {
   return (
--- client-repath/src/components/CardFriendList.jsx
+++ client-repath/src/components/CardFriendList.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Avatar } from '@mui/material/';
-import { Card } from 'react-bootstrap';
-import { red } from '@mui/material/colors';
-import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
-import { useDispatch } from 'react-redux';
-import { delFriendReq } from '../store/actionCreators/followCreator';
-=======
 import React, { useState, useEffect } from 'react';
 import { Avatar, IconButton } from '@mui/material/';
 import { Card, Modal, Button } from 'react-bootstrap';
@@ -14,31 +5,11 @@ import { red } from '@mui/material/colors';
 import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
 import { useDispatch, useSelector } from 'react-redux';
 import { delFriendReq, delOneFriend } from '../store/actionCreators/followCreator';
->>>>>>> b388d509f2790f6d8fcc670e8476111e839efe75
 
 function CardFriendList({ user }) {
   const dispatch = useDispatch()
 
   const handleDelFriend = (reqId) => {
-<<<<<<< HEAD
-    dispatch(delFriendReq(reqId))
-  }
-  return (
-    <Card style={{ border: '0px' }}>
-      <Card.Body style={{ backgroundColor: '#fef2f2', padding: '0px' }}>
-        <div className="card-container-friendlist">
-          <div className="card-left-side-friendlist d-flex align-items-center">
-            {user.imgUrl ? (
-              <Avatar className="avatar-card" alt="David" src={user.imgUrl} sx={{ width: 60, height: 60 }} variant="rounded"></Avatar>
-            ) : (
-              <Avatar className="avatar-card" alt={user.firstName} src="/static/images/avatar/1.jpg" sx={{ width: 60, height: 60 }} variant="rounded"></Avatar>
-            )}
-          </div>
-          <div className="card-right-side-friendlist d-flex flex-row align-items-center">
-            <div className="content-friendlist" style={{ width: '250px', textAlign: 'left' }}>
-              <div className="friendlist-name">
-                {user.firstName} {user.lastName}
-=======
     dispatch(delOneFriend(reqId));
   };
 
@@ -75,7 +46,6 @@ function CardFriendList({ user }) {
                   {user.firstName} {user.lastName}
                 </div>
                 <div className="friendlist-city">{user.city}</div>
->>>>>>> b388d509f2790f6d8fcc670e8476111e839efe75
               </div>
             </div>
             <div style={{ width: '100px' }} className="d-flex justify-content-center align-items-center">
@@ -84,14 +54,6 @@ function CardFriendList({ user }) {
               </IconButton>
             </div>
           </div>
-<<<<<<< HEAD
-          <div style={{ width: '100px' }} className="d-flex justify-content-center align-items-center">
-                <PersonRemoveIcon onClick={() => handleDelFriend(user.phoneNumber)} sx={{ width: 35, height: 35, color: red[500] }} />
-          </div>
-        </div>
-      </Card.Body>
-    </Card>
-=======
         </Card.Body>
       </Card>
       <Modal show={show} onHide={handleClose} centered>
@@ -121,7 +83,6 @@ function CardFriendList({ user }) {
         </Modal.Footer>
       </Modal>
     </>
->>>>>>> b388d509f2790f6d8fcc670e8476111e839efe75
   );
 }
 
